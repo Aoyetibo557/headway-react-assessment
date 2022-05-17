52907f71cb7021e7c3bc6eb4dfebbe9e55157e74
Anuoluwapo Gabriel Oyetibo <54692909+Aoyetibo557@users.noreply.github.com>
2022-05-16 21:06:07 -0400

    created apollo provider added the apollo provider logic to work with the backend

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import "./globalstyles.css";
-// import {
-//   ApolloClient,
-//   InMemoryCache,
-//   ApolloProvider,
-//   HttpLink,
-//   from
-// } from "@apollo/client";
-// import { onError } from "@apollo/client/link/error";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  HttpLink,
+  from
+} from "@apollo/client";
+
+import { onError } from "@apollo/client/link/error";
 import LandingPage from "./pages/LandingPage";
 
-// const errorLink = onError(({ graphqlErrors, networkError }) => {
-//   if (graphqlErrors) {
-//     graphqlErrors.map(({ message, location, path }) => {
-//       alert(`Graphql error ${message}`);
-//     });
-//   }
-// });
-// const link = from([errorLink, new HttpLink({ uri: "http://localhost:4000/" })]);
-// const client = new ApolloClient({
-//   cache: new InMemoryCache(),
-//   link: link
-// });
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+});
+const link = from([errorLink, new HttpLink({ uri: "http://localhost:4000/" })]);
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link
+});
 
 export default function App() {
   return (
-    <div className="App">
+    <ApolloProvider client={client} className="App">
       <LandingPage />
-    </div>
+    </ApolloProvider>
   );
 }
